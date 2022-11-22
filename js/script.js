c404c5b99edc56c3220d1543646368fdb68baa8e
SamuelNdreu <samwellsoficcial@gmail.com>
2022-11-22 16:07:22 +0100

    writing code but no sense meaning

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const userKm = document.getElementById('kilometers');
 const userAge = document.getElementById('age');
 const input = document.getElementById('button');
 
-input.addEventListener('click', function (){
+input.addEventListener('click'),
+function (){
     console.log(userKm.value);
     console.log(userAge.value);
 }
